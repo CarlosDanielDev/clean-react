5220e8fc070dc8de71df6672b9b2db4351c3b939
Carlos Daniel <killer.dk20@gmail.com>
2021-01-24 21:50:27 -0300

    test: ensure RemoteAuthentication calls http post client with correct body

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,6 +2,8 @@ import { RemoteAuthentication } from './remote-authentication'
 import { HttpPostClientSpy } from '~/data/test/mock-http-client'
 import { mockAuthentication } from '~/domain/test/mock-authentication'
 import faker from 'faker'
+import { InvalidCredentialsError } from '~/domain/errors/invalid-credentials-error'
+import { HttpStatusCode } from '~/data/protocols/http/http-response'
 
 type SutTypes = {
   sut: RemoteAuthentication
@@ -30,5 +32,14 @@ describe('Suit RemoteAuthentication', () => {
     const authenticationParams = mockAuthentication()
     await sut.auth(authenticationParams)
     expect(httpPostClientSpy.body).toEqual(authenticationParams)
+  })
+
+  test('Should Throw InvalidCredentialsError if HttpPostClient if returns 401', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.unauthorized
+    }
+    const promise = sut.auth(mockAuthentication())
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError())
   })
 })
