50cf09f29d532abe0a4f48f40b5a8381dbbf9d4f
Carlos Daniel <killer.dk20@gmail.com>
2021-01-24 21:15:57 -0300

    refactor: change imports to use module

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,6 @@
 import { RemoteAuthentication } from './remote-authentication'
-import { HttpPostClientSpy } from '../../test/mock-http-client'
-import { mockAuthentication } from '../../../domain/test/mock-authentication'
+import { HttpPostClientSpy } from '~/data/test/mock-http-client'
+import { mockAuthentication } from '~/domain/test/mock-authentication'
 import faker from 'faker'
 
 type SutTypes = {
@@ -17,15 +17,15 @@ const makeSut = (url: string = faker.internet.url()): SutTypes => {
   }
 }
 
-describe('RemoteAuthentication', () => {
-  test('Should call HttpPosyClient with correct URL', async () => {
+describe('Suit RemoteAuthentication', () => {
+  test('Should call HttpPostClient with correct URL', async () => {
     const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
     await sut.auth(mockAuthentication())
     expect(httpPostClientSpy.url).toBe(url)
   })
 
-  test('Should call HttpPosyClient with correct body', async () => {
+  test('Should call HttpPostClient with correct body', async () => {
     const { sut, httpPostClientSpy } = makeSut()
     const authenticationParams = mockAuthentication()
     await sut.auth(authenticationParams)
