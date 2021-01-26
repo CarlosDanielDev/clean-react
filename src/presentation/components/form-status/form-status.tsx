a475f389fb218bace6e59870401c5ba2aca23464
Carlos Daniel <killer.dk20@gmail.com>
2021-01-25 23:35:04 -0300

    feat: ensure Login calls validation with correct email

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -4,12 +4,11 @@ import Styles from './form-status-styles.scss'
 import Context from '~/presentation/contexts/form/form-context'
 
 const FormStatus: React.FC = () => {
-  const { state: { isLoading }, errorState: { main } } = useContext(Context)
+  const { state: { isLoading, mainError } } = useContext(Context)
   return (
     <div data-testid="error-wrap" className={Styles.errorWrap}>
       {isLoading && <Spinner className={Styles.spinner}/>}
-
-      {main && <span className={Styles.error}>{main}</span>}
+      {mainError && <span className={Styles.error}>{mainError}</span>}
     </div>
   )
 }
--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -4,10 +4,17 @@ import Context from '~/presentation/contexts/form/form-context'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 const Input: React.FC<Props> = (props: Props) => {
-  const { errorState } = useContext(Context)
-  const error = errorState[props.name]
+  const { state, setState } = useContext(Context)
+  const error = state[`${props.name}Error`]
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
+  }
+
+  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    })
   }
   const getStatus = (): string => {
     return '🔴'
@@ -19,7 +26,7 @@ const Input: React.FC<Props> = (props: Props) => {
 
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} readOnly onFocus={enableInput} />
+      <input {...props} readOnly data-testid={props.name} onFocus={enableInput} onChange={handleChange} />
       <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
     </div>
   )
