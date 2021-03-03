b97db85427d0e23c05db103fd6593f1e793262ec
Carlos Daniel <killer.dk20@gmail.com>
2021-03-03 02:11:38 -0300

    test: ensure Login preset error and hide spinner on authentication failure

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import faker from 'faker'
-import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
+import { render, RenderResult, fireEvent, cleanup, waitFor } from '@testing-library/react'
 import Login from './login'
 import { AuthenticationSpy, ValidationStub } from '~/presentation/test'
+import { InvalidCredentialsError } from '~/domain/errors'
 
 type SutTypes = {
   sut: RenderResult
@@ -133,5 +134,18 @@ describe('Login Component', () => {
     const form = sut.getByTestId('form')
     fireEvent.submit(form)
     expect(authenticationSpy.callsCount).toBe(0)
+  })
+
+  test('Should preset error if Authentication fails', async () => {
+    const { sut, authenticationSpy } = makeSut()
+    const error = new InvalidCredentialsError()
+    jest.spyOn(authenticationSpy, 'auth').mockReturnValueOnce(Promise.reject(error))
+    simulateValidSubmit(sut)
+    const errorWrap = sut.getByTestId('error-wrap')
+    await waitFor(() => errorWrap)
+    const mainError = sut.getByTestId('main-error')
+    expect(mainError.textContent).toBe(error.message)
+
+    expect(errorWrap.childElementCount).toBe(1)
   })
 })
