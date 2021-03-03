0d8163df56debe78ba472a486d3e9904fe4c642b
Carlos Daniel <killer.dk20@gmail.com>
2021-03-03 01:53:35 -0300

    refactor: prevent login to call submit more than once

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -29,9 +29,8 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
-    if (state.isLoading) {
-      return
-    }
+    if (state.isLoading) return
+
     setState({ ...state, isLoading: true })
     const { email, password } = state
     await authentication.auth({ email, password })
